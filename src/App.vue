3ef9aa1f659a6cd23942cc3eddc92a536f0c1952
ChangJoo Park <changjoo.park@partner.so>
2017-01-05 10:28:09 +0900

    Initialize Commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+  #app
+    #code-mirror
+    div {{ rawCode }}
+
+  </div>
+</template>
+
+<script>
+import CodeMirror from 'codemirror'
+
+export default {
+  name: 'app',
+  mounted: function () {
+    const appEl = document.querySelector('#code-mirror')
+    this.cm = CodeMirror(appEl, {
+      mode: 'javascript'
+    })
+    console.log(this.cm.getValue())
+  },
+  data: function () {
+    return {
+      cm: ''
+    }
+  },
+  computed: {
+    rawCode: function () {
+      if (!this.cm) {
+        return ''
+      }
+      return this.cm.getValue()
+    }
+  },
+  components: {
+  }
+}
+</script>
+
+<style>
+</style>
